16f57e396101a9bca3474057156919bd7e076ef3
BuildTools <unconfigured@null.spigotmc.org>
2015-09-05 10:28:28 +0800

    fix web service

--- index.js
+++ index.js
@@ -8,16 +8,6 @@ http.createServer(function (req, res) {
 
 console.log('Server running at http://127.0.0.1:' + process.env.PORT + '/');
 
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
-
-console.log('Server running at http://127.0.0.1:1337/');
-
-
 var client = new irc.Client('kornbluth.freenode.net', 'oktw', {
     channels: ['#oktw', '#ysitd', '#koru1130'],
     userName: 'oktw',
