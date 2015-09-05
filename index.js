a7151703093cb73931d98308a4b52f4888914a9b
BuildTools <unconfigured@null.spigotmc.org>
2015-09-05 10:19:14 +0800

    edit web service bind port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(80, "127.0.0.1");
+}).listen(process.env.PORT, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1/');
 
