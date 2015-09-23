247ba0096710ee1d620cf93cad8de467c12e22d3
james58899 <james59988@gmail.com>
2015-09-23 22:05:32 +0800

    add module title

---
+++ modules/title.js
@@ -0,0 +1,17 @@
+var request = require('request');
+
+module.exports = function (from, to, url) {
+    if (url.length === 1) {
+        url = url[0]
+    }else{
+        url = url[1]
+    }
+    request(url,{timeout: 1500}, function(error, response, body) {
+        if (!error && response.statusCode == 200 && body.match(/<title>.*<\/title>/i)) {
+            var title = body.match(/<title>.*<\/title>/i).toString().replace(/<\/?title>/ig, '');
+            say(from, to, '[Title] ' + title);
+        }
+    })
+}
+
+module.exports.init = {name:'title', help:'Analysis url title'}
