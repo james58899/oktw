32405e5b442b387d6c8bc43ba72532dcbf4c81a4
james58899 <james59988@gmail.com>
2015-09-25 21:57:57 +0800

    add command admin reload

--- modules/admin.js
+++ modules/admin.js
@@ -30,6 +30,7 @@ module.exports = function (from, to, args) {
                 break;
             case 'reload':
                 oktw.reload();
+                oktw.say(from, to, 'Bot reloaded!'
                 break;
         }
     })
