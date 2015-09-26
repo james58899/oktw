7889519133b94deb2fb375fdecfd7be480e62b83
James58899 <james58899@oktw.me>
2015-09-26 12:40:31 +0000

    fix Excess Flood

--- modules/math.js
+++ modules/math.js
@@ -3,7 +3,10 @@ var math = require('mathjs');
 module.exports = function (from, to, args) {
     var i = args.join(' ').replace('c ', '');
     /*global oktw*/
-    oktw.say(from, to, math.eval(i));
+    console.log(math.eval(i), typeof math.eval(i));
+    if (typeof math.eval(i) === 'number') {
+        oktw.say(from, to, math.eval(i));
+    }
 };
 
 module.exports.info = {name:'math', example:'.c 1+1', command:'c'};
