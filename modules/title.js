6e8179c9b2b5e51858309d279a6743fbc2604e58
James58899 <james58899@oktw.me>
2015-09-27 12:08:55 +0000

    fix fetch facebook and ptt(18+) title

--- modules/title.js
+++ modules/title.js
@@ -7,7 +7,15 @@ module.exports = function (from, to, url) {
     }else{
         url = url[1];
     }
-    request(url,{timeout: 1500}, function(error, response, body) {
+    var options = {
+        url: url,
+        headers: {
+            'User-Agent': 'chrome32 on win8 Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36',
+            'Cookie': 'over18=1'
+        },
+        timeout: 1500
+    };
+    request(options, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             $ = cheerio.load(body);
             var title =$('title').text();
