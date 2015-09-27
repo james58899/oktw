0cec30e3680b5b3620495f66c44abab9eef7e1e1
James58899 <james58899@oktw.me>
2015-09-27 10:09:12 +0000

    add commanda .me and fix bug

--- modules/unicode.js
+++ modules/unicode.js
@@ -1,7 +1,7 @@
 module.exports = function (from, to, args) {
     if (args[1].match(/^\\u/)) {
         /*global oktw*/
-        oktw.say(from, to, unescape(args[1].replace(/\\u/g,"%u")));
+        oktw.say(from, to, unescape(args[1].match(/\\u[a-z0-9]{4}/i).toString().replace(/\\u/i,"%u")));
     }else{
         oktw.say(from, to, '\\u' + ('00' + args[1].charCodeAt(0).toString(16)).slice(-4));
     }
