e7213a26fd9bf94672f4412b9824b8b15b6858cc
james58899 <james59988@gmail.com>
2015-09-28 17:53:16 +0800

    add command pagespeed

---
+++ modules/pagespeed.js
@@ -0,0 +1,13 @@
+var request = require('request');
+
+module.exports = function (from, to, args) {
+    var url = 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?&locale=zh_TW&screenshot=false&fields=ruleGroups&key=AIzaSyDjYeBKwBrGq2I6FhxJj1il6R40buIKECo&url=' + args[1];
+    request(url, function(err, response, body) {
+        if (!err && response.statusCode == 200) {
+            /*global oktw*/
+            oktw.say(from, to, JSON.parse(body).ruleGroups.SPEED.score);
+         }
+    });
+};
+
+module.exports.info = {name:'pagespeed', example:'.ps www.oktw.tw', command:'ps'};
