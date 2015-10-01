e6ba4e1970c838010b50fbb2564b072ddddea4b7
james58899 <james59988@gmail.com>
2015-10-02 02:48:49 +0800

    save admin and ignore list

--- modules/admin.js
+++ modules/admin.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 module.exports = function (from, to, args) {
     /*global oktw*/
     oktw.checkAdmin(from, function(admin) {
@@ -9,7 +11,10 @@ module.exports = function (from, to, args) {
             case 'op':
                 if(oktw.admin.indexOf(args[2]) < 0){
                     oktw.admin.push(args[2]);
-                    oktw.say(from, to, args[2] + ' 設為管理員！');
+                    fs.writeFile('./data/admin.json', JSON.stringify(oktw.admin), function(err) {
+                        if (err) throw err;
+                        oktw.say(from, to, args[2] + ' 設為管理員！');
+                    })
                 }else{
                     oktw.say(from, to, args[2] + ' 已經是管理員了');
                 }
@@ -17,7 +22,10 @@ module.exports = function (from, to, args) {
             case 'deop':
                 if(oktw.admin.indexOf(args[2]) > -1) {
                     oktw.admin.splice(oktw.admin.indexOf(args[2]), 1);
-                    oktw.say(from, to, '取消 ' + args[2] + ' 的管理員權限！');
+                    fs.writeFile('./data/admin.json', JSON.stringify(oktw.admin), function(err) {
+                        if (err) throw err;
+                        oktw.say(from, to, '取消 ' + args[2] + ' 的管理員權限！');
+                    })
                 }else{
                     oktw.say(from, to, args[2] + ' 不是管理員');
                 }
@@ -25,7 +33,10 @@ module.exports = function (from, to, args) {
             case 'ban':
                 if(oktw.ignore.indexOf(args[2]) < 0) {
                     oktw.ignore.push(args[2]);
-                    oktw.say(from, to, '忽略 ' + args[2]);
+                    fs.writeFile('./data/ignore.json', JSON.stringify(oktw.ignore), function(err) {
+                        if (err) throw err;
+                        oktw.say(from, to, '忽略 ' + args[2]);
+                    })
                 }else{
                     oktw.say(from, to, args[2] + ' 已在忽略名單中');
                 }
@@ -33,7 +44,10 @@ module.exports = function (from, to, args) {
             case 'unban':
                 if(oktw.ignore.indexOf(args[2]) > -1){
                     oktw.ignore.splice(oktw.ignore.indexOf(args[2]), 1);
-                    oktw.say(from, to, '取消忽略 ' + args[2]);
+                    fs.writeFile('./data/ignore.json', JSON.stringify(oktw.ignore), function(err) {
+                        if (err) throw err;
+                        oktw.say(from, to, '取消忽略 ' + args[2]);
+                    })
                 }else{
                     oktw.say(from, to, args[2] + ' 沒有被忽略');
                 }
