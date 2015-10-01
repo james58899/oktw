259e9b26cbdf5ce5b8a636b424b5a9f46ecebdc6
james58899 <james59988@gmail.com>
2015-10-02 02:18:20 +0800

    change UA

--- modules/title.js
+++ modules/title.js
@@ -12,7 +12,7 @@ module.exports = function (from, to, uri) {
     var options = {
         url: encodeURI(uri.href),
         headers: {
-            'User-Agent': 'chrome32 on win8 Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36',
+            'User-Agent': 'request',
             'Cookie': 'over18=1',
             'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3'
         },
