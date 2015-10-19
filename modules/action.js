676ba65e6800cedecae69d18be344f084117ac7f
James58899 <james58899@oktw.me>
2015-10-19 04:07:36 +0000

    add sand to channel

--- modules/action.js
+++ modules/action.js
@@ -1,8 +1,14 @@
 module.exports = function (from, to, args) {
+    /*global oktw*/
     if (args.length > 1) {
-        /*global oktw*/
-        oktw.say(from, to, unescape('\u0001ACTION ' + args.join(' ').replace('me ', '') + '\u0001'));
+        if (Object.keys(oktw.irc.chans).indexOf(args[1]) > -1) {
+            var chan = args[1];
+            var args = args.slice(2);
+            oktw.say(from, chan, unescape('\u0001ACTION ' + args.join(' ') + '\u0001'));
+        }else{
+            oktw.say(from, to, unescape('\u0001ACTION ' + args.slice(1).join(' ') + '\u0001'));
+        }
     }
 };
 
-module.exports.info = {name:'action', example:'.me Hi', command:'me'};
+module.exports.info = {name:'action', example:'.me Hi or .me #oktw Hi', command:'me'};
--- modules/say.js
+++ modules/say.js
@@ -1,8 +1,14 @@
 module.exports = function (from, to, args) {
+    /*global oktw*/
     if (args.length > 1) {
-        /*global oktw*/
-        oktw.say(from, to, args.join(' ').replace('say ', ''));
+        if (Object.keys(oktw.irc.chans).indexOf(args[1]) > -1) {
+            var chan = args[1];
+            var args = args.slice(2);
+            oktw.say(from, chan, args.join(' '));
+        }else{
+            oktw.say(from, to, args.slice(1).join(' '));
+        }
     }
 };
 
-module.exports.info = {name:'say', example:'.say Hello World!', command:'say'};
+module.exports.info = {name:'say', example:'.say Hello World! or .say #oktw Hello World!', command:'say'};
