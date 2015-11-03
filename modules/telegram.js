9857d71c4921b8e4630eaa3dbba676ae9bc36796
James58899 <james58899@oktw.me>
2015-11-03 15:58:59 +0000

    update telegram bridge

--- modules/telegram.js
+++ modules/telegram.js
@@ -1,11 +1,10 @@
 var TelegramBot = require('node-telegram-bot-api');
 var util = require('util');
-var request = require('request');
-var imgur = require('imgur-node-api')
+var imgur = require('imgur');
 
 var token = '165999018:AAGmDVig2IDY7lmiwUMnW2VgR53PUNEGwVU';
-var tg = new TelegramBot(token, {polling: true});
-imgur.setClientID('32cd79f0cb986fc');
+var tg = new TelegramBot(token, {polling:{timeout:5, interval:500}});
+imgur.setClientId('32cd79f0cb986fc');
 
 tg.on('message', function (msg) {
     if(msg.text) {
@@ -13,16 +12,11 @@ tg.on('message', function (msg) {
         oktw.irc.say('#ysitd', util.format('<%s>: %s', msg.from.username, msg.text));
     }
     if(msg.photo) {
-        var file = msg.photo[msg.photo.length-1].file_id;
-        request('https://api.telegram.org/bot' + token + '/getFile?file_id=' + file,function(error, response, body){
-            if (!error && response.statusCode == 200) {
-                var json=JSON.parse(body);
-                imgur.upload('https://api.telegram.org/file/bot'+token+'/'+json.result.file_path,function(err,res){
-                    if(!err){
-                        oktw.irc.say('#ysitd', res.data.link);
-                    }
-                });
-            }
+        var fileId = msg.photo[msg.photo.length-1].file_id;
+        tg.getFileLink(fileId).then(function(url) {
+            imgur.uploadUrl(url).then(function(res){
+                oktw.irc.say('#ysitd', util.format('<%s>: %s', msg.from.username, res.data.link));
+            });
         });
     }
 });
