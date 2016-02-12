5c2266a953e4d9a0c33a159f1d6b46b52e11b632
james59988 <james58899@oktw.tw>
2016-02-12 17:26:45 +0000

    add telegram sticker upload

--- modules/telegram.js
+++ modules/telegram.js
@@ -7,7 +7,7 @@ var token = oktw.config.tgKey;
 var tg = new TelegramBot(token, {
     polling: {
         timeout: 5,
-        interval: 500
+        interval: 100
     }
 });
 imgur.setClientId('41ad90f344bdf2f');
@@ -45,6 +45,15 @@ tg.on('message', function(msg) {
             console.log('%s => Telegram: %s', msg.from.username, msg.caption.replace(/\s/g, ' '));
             oktw.irc.say('#ysitd', util.format('<%s>: %s', msg.from.username, msg.caption.replace(/\s/g, ' ')));
         }
+        if (msg.sticker) {
+            tg.getFileLink(msg.sticker.file_id).then(function(url) {
+                imgur.uploadUrl(url).then(function(res) {
+                    oktw.irc.say('#ysitd', util.format('<%s>: %s', msg.from.username, res.data.link));
+                }).catch(function (err) {
+                    console.error(err.message);
+                });
+            });
+        }
     }
 });
 
@@ -53,6 +62,11 @@ module.exports = function(from, to, message) {
         tg.sendMessage(tgChatID, util.format('<%s>: %s', from, message.toString().replace(/((?:\u0003\d\d?,\d\d?|\u0003\d\d?|\u0002|\u001d|\u000f|\u0016|\u001f))/g, '')));
     }
 };
+
+oktw.irc.on('action', function(from, to, text, message) {
+    tg.sendMessage(tgChatID, util.format('<%s>: /me %s', from, text.toString().replace(/((?:\u0003\d\d?,\d\d?|\u0003\d\d?|\u0002|\u001d|\u000f|\u0016|\u001f))/g, '')));
+});
+
 module.exports.info = {
     name: 'telegram',
     rawcommand: /.*/
